1eaee4b5e83767cc0078b171571238f89d357945
Violeta Karadzhova <violeta.karadzhova@gmail.com>
2026-08-02 13:18:31 +0300

    chore: setup project infrastructure, databases, and dependencies

---
+++ main-app/src/main/java/com/sunnyvet/main/MainAppApplication.java
@@ -0,0 +1,17 @@
+package com.sunnyvet.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableCaching
+@EnableScheduling
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
---
+++ microservice/src/test/java/com/sunnyvet/microservice/MicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunnyvet.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
